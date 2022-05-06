1a0f29b66c55aa8df3237e1cc4495589aed45b59
Fabio Mangolini <fabio@meltinbit.com>
2022-05-06 16:35:17 +0200

    ...

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from '@shopify/app-bridge-react'
 
-/* ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-) */
-
-const config = { apiKey: '24932ce9a3fe577f2522782ad88346ed', host: new URL(location).searchParams.get("host"), forceRedirect: true }
+const config = {
+	apiKey: import.meta.env.VITE_API_KEY,
+	host: new URL(location).searchParams.get("host"),
+	forceRedirect: true
+}
 
 ReactDOM.render(
 	<Provider config={config}>
